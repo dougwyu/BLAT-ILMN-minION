5b0308cd8eb00ac9ca3a79b50be8d01ae57462a2
dougwyu <dougwyu@gmail.com>
2017-08-25 21:40:45 +0100

    awk the psl output to keep only reads that have > some threshold of matched nucleotides
    e.g. >=213 matches out of 251 nucleotides.  213/251 = 85% similarity.

--- BLAT_reads.sh
+++ BLAT_reads.sh
@@ -58,17 +58,38 @@ ssh grace
 ssh hpc.uea.ac.uk
 # interactive -x # exclusive job, which is alternative to interactive -R
 interactive -R "rusage[mem=4000]" -M 4000 # 4000=4GB RAM, which is the max for an hpc interactive job
-module load blat # v. 36
-PARALLEL=~/scripts/parallel-20170722/bin/parallel
-ILMNFAS=plantskims/prepare_forBWA/prepare_forblast/
+# module load blat # v. 36
+PATH=$PATH:/gpfs/home/b042/scripts/pblat/ # pblat v.35
 WORKDIR=~/pollen_minION/
+MINIONDIR=minION/
+ILMNFAS=plantskims/prepare_forBWA/prepare_forblast/
+BLATDIR=blatout/
 TILE=10
 STEP=5
-TARGET=12  # minION reads
-QUERY=E3  # Illumina reads = plant skims that have been adapter-trimmed, denoised, merged, and converted to fasta
+MATCH=213 # 85% of 251 is 213.  in other words, I am asking for the illumina read to have at least 85% similarity to the minION read
 NPROC=$(grep -c processor /proc/cpuinfo)
 
-cd ${WORKDIR}; blat -tileSize=${TILE} -stepSize=${STEP} -noHead ${WORKDIR}minION/barcode${TARGET}_all_pass.fasta ${WORKDIR}${ILMNFAS}${QUERY}_bfc_trimed_uniques.fasta.gz ${WORKDIR}output_Plate1_${QUERY}.psl
+cd ${WORKDIR}
+declare -a illuminarray=(A2 A3 A5 B2 B4 C2 C4 C6 C12 E3) # # Illumina reads = plant skims that have been adapter-trimmed, bfc-denoised, merged, dereplicated, and converted to fasta
+declare -a minionarray=(11 12) # minION reads, all barcodes that successfully sequenced
+
+# e.g. first element in array
+# ${illuminarray[0]}
+# ${minionarray[0]}
+
+i=1
+m=1
+
+gunzip ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta.gz
+
+pblat -tileSize=${TILE} -stepSize=${STEP} -threads=${NPROC} -noHead ${WORKDIR}${MINIONDIR}barcode${m}_all_pass.fasta ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl
+
+gzip ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta
+
+awk -v sim=${MATCH} '$1 >= sim { print }' ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl | gzip > ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz
+
+wc -l ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz >> psl_wc-l.txt
+
 # barcode 12 should have many good matches to A5 Papaver rhoeas
 # barcode 12 should have 0 good matches to B5 Papaver somniferum (but is a congener of A5 so some false positives are expected)
 # barcode 12 should have 0 good matches to A8 Tripleurospermum maritimum
@@ -82,36 +103,70 @@ cd ${WORKDIR}; blat -tileSize=${TILE} -stepSize=${STEP} -noHead ${WORKDIR}minION
 # mkdir output_Plate1_${QUERY}_output/
 # cat output_Plate1_${QUERY}.psl | awk -v PFX=~/pollen_minION/output_Plate1_${QUERY}_output/ '{ file=PFX"/"$14".psl"; print $0>file; }'  # creates as many files as there are minION reference reads
 
+ls blatout/
+bpeek 207901
 
-#### bsub version
 
+########## BSUB START ###################################################
 #!/bin/sh
 #BSUB -q short-eth  # short-eth has 24 hrs, mellanox-ib 7 days
-#BSUB -n 12  # short-eth has 24 cores, add #BSUB -x # if i want an exclusive
-#BSUB -J blatmin  # 10 chars max
+#BSUB -n 24  # short-eth has 24 cores, add #BSUB -x # if i want an exclusive
+#BSUB -J blatminse  # 10 chars max
 #BSUB -B
 #BSUB -N
-#BSUB -oo blatmin.out
-#BSUB -eo blatmin.err
-#BSUB -R "rusage[mem=3000]" # 3000 = 3 GB RAM. BLAT uses < 500 MB
-#BSUB -M 4000
-
+#BSUB -oo blatminse.out
+#BSUB -eo blatminse.err
+#BSUB -R "rusage[mem=3800]" # 3800 = 3.8 GB RAM. BLAT uses < 500 MB
+#BSUB -M 3800
 
 . /etc/profile
-module load blat # v.36
-PARALLEL=~/scripts/parallel-20170722/bin/parallel
-ILMNFAS=plantskims/prepare_forBWA/prepare_forblast/
+# module load blat # v.36
+PATH=$PATH:/gpfs/home/b042/scripts/pblat/ # pblat v.35  parallel blat
 WORKDIR=~/pollen_minION/
+MINIONDIR=minION/
+ILMNFAS=plantskims/prepare_forBWA/prepare_forblast/
+BLATDIR=blatout/
 TILE=10
 STEP=5
-TARGET=12  # minION reads
-QUERY=E3  # Illumina reads = plant skims that have been adapter-trimmed, denoised, merged, and converted to fasta
+MATCH=213 # 85% of 251 is 213.  in other words, I am asking for the illumina read to have at least 85% similarity to the minION read
+NPROC=$(grep -c processor /proc/cpuinfo)
 
-blat -tileSize=${TILE} -stepSize=${STEP} -noHead ${WORKDIR}minION/barcode${TARGET}_all_pass.fasta ${WORKDIR}${ILMNFAS}${QUERY}_bfc_trimed_uniques.fasta.gz ${WORKDIR}output_Plate1_${QUERY}.psl
+cd ${WORKDIR}
+mkdir ${BLATDIR}
 
+declare -a illuminarray=(A1 A2 A3 B12 A4 A5 A12B5B6 A7A8 A9 A10 A11 B1 B2 C4 B3 B4 B7 B8 B9 B10 B11 C1 C2 C3 C5 C6C7 C8 C9 C10 C11 C12 D1 D2 D8 D3 D4 D5 D6 D7 D10 D9 D11 D12 E1 E2 E3 E4 E5 E6 E7 E8) # # Illumina reads = plant skims that have been adapter-trimmed, bfc-denoised, merged, dereplicated, and converted to fasta
+declare -a minionarray=(01 02 03 04 06 08 09 10 11 12) # minION reads, all barcodes that successfully sequenced
 
-# to parse a blat *.psl file so that the nth field has a value > 220, use something like this:
-# awk -F "\t" '{ $n > 220 }' *.psl | long-mapped_illuminareads.psl
+rm -f psl_wc-l_Minsimilarity_${MATCH}.txt
+touch psl_wc-l_Minsimilarity_${MATCH}.txt
+echo "Illumina minION    Good_reads"  >> psl_wc-l_Minsimilarity_${MATCH}.txt
 
-# can i pipe the output of blat into awk and then into gzip?
-# blat | awk | gzip
+for i in "${illuminarray[@]}"
+do
+     for m in "${minionarray[@]}"
+     do
+          echo "Illumina reference ${i}"
+          echo "minION query ${m}"
+
+          gunzip ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta.gz
+
+          pblat -tileSize=${TILE} -stepSize=${STEP} -threads=24 -noHead ${WORKDIR}${MINIONDIR}barcode${m}_all_pass.fasta ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl
+
+          gzip ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta
+
+          awk -v sim=${MATCH} '$1 >= sim { print }' ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl | gzip > ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz
+
+          rm ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl
+
+          echo "${i}     ${m} $(wc -l ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz)" >> psl_wc-l_Minsimilarity_${MATCH}.txt
+     done
+done
+########## BSUB END ###################################################
+
+# possibly could use anonymous named pipes to gzip psl output file
+
+# (awk -v sim=${MATCH} '$1 >= sim { print }' | gzip > ) is applied to the output before being saved to disk
+
+# blat -tileSize=${TILE} -stepSize=${STEP} -noHead ${WORKDIR}${MINIONDIR}barcode${m}_all_pass.fasta ${WORKDIR}${ILMNFAS}${i}_bfc_trimed_uniques.fasta.gz (awk -v sim=${MATCH} '$1 >= sim { print }' | gzip > ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz)
+#
+# wc -l ${WORKDIR}${BLATDIR}output_ILMNREF_${i}_MINIONBARCODE_${m}.psl.gz >> psl_wc-l.txt
